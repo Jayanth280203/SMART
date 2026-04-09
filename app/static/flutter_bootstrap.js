aba1e73cde425aaa3b1a28840263316f2a24bc29
Jayanth280203 <jayanthr239@gmail.com>
2026-04-09 13:26:10 +0530

    fix: mass align block to district geolocations based on assembly hierarchy

--- app/static/flutter_bootstrap.js
+++ app/static/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "748217027"
+    serviceWorkerVersion: "1426683370"
   }
 });
