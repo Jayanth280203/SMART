0e32f23bdabcd8c350d7601e788de2de4646a746
Jayanth280203 <jayanthr239@gmail.com>
2026-04-09 11:16:39 +0530

    build: prepare updated flutter web files for Render deployment

--- app/static/flutter_service_worker.js
+++ app/static/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3190373f15f30b7fcd5179cbaff1e528
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "734720230ee4b68f989fc3b08e4594e8",
+"flutter_bootstrap.js": "e904c8ef5556082ca87828c41630d45b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
